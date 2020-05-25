f81c6c7266739ca720b25ec59f3921871688a601
Alexander Briasco-Stewart <astewart.is1@gmail.com>
2020-05-25 10:50:40 -0700

    Dockerizing Frontend -- in progress

--- host_start_backend.sh
+++ host_start_backend.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
-
-docker-compose rm -f tracing_database backend
+echo "===== Removing old containers ====="
+docker-compose rm -f database backend
+echo ""
+echo "===== Rebuilding backend ====="
 ./backend/gradlew build
+echo ""
+echo "===== Spinning up new containers ====="
 docker-compose up --build --force-recreate
