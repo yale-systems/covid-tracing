d81b8a30c9a4460b5b4403b4b4589ac1b5181c67
maxtheaxe <max.perrello@gmail.com>
2020-07-14 20:07:13 -0400

    fixed basic exception handling

--- backend/src/main/java/org/yale/registry/research/exceptions/RESTExceptionHandler.java
+++ backend/src/main/java/org/yale/registry/research/exceptions/RESTExceptionHandler.java
@@ -8,11 +8,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+// Reference: https://docs.spring.io/spring/docs/5.2.7.RELEASE/spring-framework-reference/web.html#mvc-ann-rest-exceptions
+
 //@Order(Ordered.HIGHEST_PRECEDENCE) // need to verify this should be done
-@ControllerAdvice
+@ControllerAdvice // @RestControllerAdvice not needed--we are handling responses manually
 public class RESTExceptionHandler extends ResponseEntityExceptionHandler {
 
     // Custom Response Entity
@@ -21,12 +24,23 @@ public class RESTExceptionHandler extends ResponseEntityExceptionHandler {
     }
 
     // Custom Exception Handlers
+    // beware of runtime exceptions vs http client error exceptions
+    // https://stackoverflow.com/a/55308054/4513452
+
+    // Generic (all otherwise unhandled) Exception Handler
+    @ExceptionHandler(Exception.class)
+    protected ResponseEntity<?> handleGenericException(
+            Exception ex, WebRequest request) {
+        String error = "Unknown error occurred";
+        return buildResponseEntity(new APIError(HttpStatus.BAD_REQUEST, error, ex));
+    }
 
     // Resource Not Found Exception
     @ExceptionHandler(ResourceNotFoundException.class)
-    protected ResponseEntity<Object> handleResourceNotFoundException(
-            ResourceNotFoundException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+    protected ResponseEntity<?> handleResourceNotFoundException(
+            ResourceNotFoundException ex, WebRequest request) {
         String error = "Resource not found";
+        System.out.println(error);
         return buildResponseEntity(new APIError(HttpStatus.NOT_FOUND, error, ex));
     }
 
