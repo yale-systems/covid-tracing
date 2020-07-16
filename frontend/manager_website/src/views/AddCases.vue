f07332b3f221b10f8881b36353471f09fdaf1f23
ivf <ivy.fan@yale.edu>
2020-07-16 16:13:50 -0400

    add more fields to manager manual add patient

--- frontend/manager_website/src/views/AddCases.vue
+++ frontend/manager_website/src/views/AddCases.vue
@@ -1,45 +1,68 @@
 <template>
-    <v-stepper v-model="patientNum">
-        <v-stepper-items>
-          <v-stepper-content
-            v-for="n in totalPatientNum"
-            :key="`${n}-content`"
-            :step="n"
-          >
-            <v-container fluid>
-            <!-- Patient form goes here -->
-            <v-card class="overflow-y-auto">
-                <NewCaseForm />
-            </v-card>
-            <v-row>
-              <v-btn 
-                @click="prevStep(n)">See Previous Case</v-btn>
-              <v-spacer></v-spacer>
-              <v-btn
-                color="primary"
-                @click="nextStep(n)"
+  <div>
+    <v-row>
+      <v-col cols=3>
+        <AddPatientSidebar v-model="smolPatientNum" />
+      </v-col>
+      <v-col>
+        <v-stepper v-model="patientNum">
+            <v-stepper-items>
+              <v-stepper-content
+                v-for="n in totalPatientNum"
+                :key="`${n}-content`"
+                :step="n"
               >
-                Add Case
-              </v-btn>
-            </v-row>
-            </v-container>
-          </v-stepper-content>
-        </v-stepper-items>
-      </v-stepper>
+                <v-container fluid>
+                  <div> hello </div>
+                  <v-card class="overflow-y-auto">
+                      <NewCaseForm />
+                  </v-card>
+                <v-row>
+                  <v-btn 
+                    @click="prevStep(n)">See Previous Case</v-btn>
+                  <v-spacer></v-spacer>
+                  <v-btn
+                    color="primary"
+                    @click="nextStep(n)"
+                  >
+                    Add Case
+                  </v-btn>
+                </v-row>
+                </v-container>
+              </v-stepper-content>
+            </v-stepper-items>
+          </v-stepper>
+        </v-col>
+      </v-row>
+    </div>
 </template>
 
 <script> 
 import NewCaseForm from '@/components/NewCaseForm'
+import AddPatientSidebar from '@/components/AddPatientSidebar'
+
 export default {
     name: "AddCases",
     components: {
-        NewCaseForm
+        NewCaseForm,
+        AddPatientSidebar
     },
     data: () => {
         return {
             patientNum: 1,
             totalPatientNum: 1
         }
+    },
+    computed: {
+      smolPatientNum: {
+        get() {
+          return this.patientNum - 1
+        }, 
+        set(newVal) {
+          console.log(newVal)
+          this.patientNum = newVal + 1
+        }
+      }
     },
     methods: {
         nextStep(n) {
@@ -64,5 +87,4 @@ export default {
 </script>
 
 <style>
-
 </style>
